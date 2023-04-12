45d6ed9f3e8674efe9446903552db2aaf6feca47
Faith-Omenta <faithbomenta@gmail.com>
2023-04-12 17:10:14 +0300

    primary

--- length.js
+++ length.js
@@ -6,10 +6,3 @@ let str="I love JavaScript";
 let fam=str.length;
 console.log(fam);
 
-let str1="I am a christian";
-let drm=str1.length;
-console.log(drm);
-
-let str2="It is too cold";
-let trt=str2.length;
-console.log(trt);
--- slice.js
+++ slice.js
@@ -10,18 +10,3 @@ const txt="NaomKeruboOmenta";
 let mom=txt.slice(0,11);
 console.log(mom);
 
-const mss="Young lady";
-const yng=mss.slice(0,6);
-console.log(yng);
-
-const sentence="This is a planet";
-let x=sentence.slice(5,14);
-console.log(x);
-
-const heart="I love watching historical";
-let parents=heart.slice(11,25);
-console.log(parents);
-
-const family="I love my siblings";
-let siblings=family.slice(11,18);
-console.log(siblings)
